80a0696e96b9dbaee106987d37877f63d327653c
Shakthi Jashwanth <148980747+Shaq16@users.noreply.github.com>
2025-02-22 05:54:45 +0000

    Math for DSA

---
+++ 02_basic_math/math.py
@@ -0,0 +1,20 @@
+# Digit Concepts
+
+# 1.Digit Extraction
+num=int(input("Enter the number:"))
+def digit_ext(num):
+    digit=num%10
+    return digit
+
+# while(num>0):
+#     print(digit_ext(num))
+#     num=num//10
+
+#2.Reversal of digits
+rev=0
+def reversal(num):
+    rev=rev*10+print(digit_ext(num))
+
+while(num>0):
+    reversal(num)
+    num=num//10
