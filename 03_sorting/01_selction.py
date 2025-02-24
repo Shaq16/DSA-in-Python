18748e43416ded0f40deaaa3ce0738cb6c174f0f
Shakthi Jashwanth <148980747+Shaq16@users.noreply.github.com>
2025-02-24 04:00:15 +0000

    Sorting techniques

---
+++ 03_sorting/01_selction.py
@@ -0,0 +1,11 @@
+# 1.Selection Sort
+
+arr=[1,5,2,0,6,8,3,7]
+for i in range(len(arr)-1):
+    min=i
+    for j in range(i+1,len(arr)):
+        if arr[min]>arr[j]:
+            min=j
+    arr[i],arr[min]=arr[min],arr[i]
+print(arr)
+
---
+++ 03_sorting/02_bubble.py
@@ -0,0 +1,10 @@
+# 2.Bubble sort
+
+arr=[1,5,2,4,3,7,0,9]
+
+for i in range(len(arr)):
+    for j in range(len(arr)-i-1):
+        if arr[j]>arr[j+1]:
+            arr[j],arr[j+1]=arr[j+1],arr[j]
+
+print(arr)
